5ccd5e1f74f8b93172ce481bbfd53f89ebcd60a3
julianogueira-ui <julia.nogueira@aluno.ifsp.edu.br>
2026-05-13 17:26:35 -0300

    Refactor form submission event handler

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const formulario = document.getElementById('meuformulario');
 
   formulario.addEventListener('submit', function (evento){
     evento.preventDefault();
+    
     const bolo = {
     bolo: document.getElementById('bolo').value,
     cobertura: document.getElementById('cobertura').value,
@@ -16,13 +17,14 @@ const formulario = document.getElementById('meuformulario');
         <div class="bolo">
             <img src="${bolo.foto}" alt="${bolo.bolo}" onerror="this.src='https://via.placeholder.com/300x200?text=Sem+Foto'">
             <h3>${bolo.bolo}</h3>
-            <p><strong>Cobertura:</strong> ${bolo.cobertura}</p>
-            <p><strong>Ingredientes do Bolo:</strong> ${bolo.ingredientebolo}</p>
-            <p><strong>Modo de Preparo do Bolo:</strong> ${bolo.preparobolo}</p>
-            <p><strong>Ingredientes da Cobertura:</strong> ${bolo.ingredientecobertura}</p>
-            <p><strong>Modo de Preparo da Cobertura:</strong> ${bolo.preparocobertura}</p>
+            <p><strong>Cobertura: </strong> ${bolo.cobertura}</p>
+            <p><strong>Ingredientes do Bolo: </strong> ${bolo.ingredientebolo}</p>
+            <p><strong>Modo de Preparo do Bolo: </strong> ${bolo.preparobolo}</p>
+            <p><strong>Ingredientes da Cobertura: </strong> ${bolo.ingredientecobertura}</p>
+            <p><strong>Modo de Preparo da Cobertura: </strong> ${bolo.preparocobertura}</p>
         </div>
     `;
+    
     colecao.innerHTML+=cardHTML;
     formulario.reset()
 
